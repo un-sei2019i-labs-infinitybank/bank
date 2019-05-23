d49a82fe2ebe88d8f938b3bddbca622f9ebedbaf
ccarranzac <ccarranzac@unal.edu.co>
2019-05-22 20:20:54 -0500

    test

---
+++ FizzBuzz.java
@@ -0,0 +1,24 @@
+class Main {
+
+   
+   public static void main(String[] args) {
+      
+       for(int i=1; i<=1000; i++ ){
+           //System.out.println(i);
+       if(i%3==0 && i%5==0){
+           System.out.println("FizzBuzz");
+       }    
+      
+       else 
+       if(i%3==0){
+           System.out.println("Fizz");
+       }
+       
+      
+       }
+       
+       
+       
+       
+   }
+   }
