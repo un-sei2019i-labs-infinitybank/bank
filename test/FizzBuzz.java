c2dc65dcb18206689dfe18d2584ff20cafae052c
adrcana <adrcana@gmail.com>
2019-05-23 17:04:43 -0500

    ingreso tercer operacion algoritmo

--- test/FizzBuzz.java
+++ test/FizzBuzz.java
@@ -13,6 +13,14 @@ class Main {
        if(i%3==0){
            System.out.println("Fizz");
        }
+
+       else if(i%5==0){
+          System.out.println("Buzz");
+        
+       }
+       else{
+           System.out.println(i);
+       }
        
       
        }
