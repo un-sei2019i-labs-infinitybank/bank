196e54d57c096010fa56f6c4413ade6991fdbfb0
adrcana <adrcana@gmail.com>
2019-05-27 21:21:42 -0500

    creacion de SQlite

---
+++ mobile-app/app/src/main/java/com/example/bank_app/AdminSQLite.java
@@ -0,0 +1,25 @@
+package com.example.bank_app;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class AdminSQLite extends SQLiteOpenHelper {
+    public AdminSQLite(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table users(code int primary key autoincrement, user text, password text)");
+        db.execSQL("insert into users values('admin', 'admin')");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("create table users(code int primary key autoincrement, user text, password text)");
+        db.execSQL("insert into users values('admin', 'admin')");
+
+    }
+}
--- mobile-app/app/src/main/java/com/example/bank_app/MainActivity.java
+++ mobile-app/app/src/main/java/com/example/bank_app/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.bank_app;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et_user, et_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_user =(EditText)findViewById(R.id.txt_user);
+        et_password =(EditText)findViewById(R.id.txt_pass);
+    }
+
+    //metodo login de usuario
+
+    public void Ingresar(View view){
+
+        AdminSQLite admin = new AdminSQLite(this, "gerence", null, 1);
+        SQLiteDatabase db = admin.getWritableDatabase();
+
+        String user = et_user.getText().toString();
+        String password = et_password.getText().toString();
+
+        Cursor fila = db.rawQuery
+                ("select user, password from users where user='" + user + "'and password='" + password+"'",null);
+
+
+           if(fila.moveToFirst()){
+
+                String usua=fila.getString(0);
+                String pass=fila.getString(1);
+
+                if(user.equals(usua) && password.equals(pass)){
+                    Intent ven = new Intent(this, MainIndex.class);
+                    startActivity(ven);
+
+                    et_user.setText("");
+                    et_password.setText("");
+                }else {
+
+                    Toast.makeText(this, "El Usuario es incorrecto", Toast.LENGTH_SHORT).show();
+                }
+
+            }else{
+                Toast.makeText(this, "El Usuario no existe", Toast.LENGTH_SHORT).show();
+            }
+
     }
 }
